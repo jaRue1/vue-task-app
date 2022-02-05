5fa19f3250e3bfc79742338e10626545bff224ad
jaRue1 <jaruejohnson@outlook.com>
2022-02-05 14:54:49 -0500

    added more stuff

---
+++ src/components/Header.vue
@@ -0,0 +1,29 @@
+<template>
+  <header>
+    {{ title }}
+    <Button text='Add' color='green' />
+    <!-- <Button text='Update' color='blue' /> -->
+    <!-- <Button text='Delete' color='red' /> -->
+  </header>
+</template>
+
+<script>
+import Button from "./Button.vue";
+export default {
+  name: "Header",
+  props: {
+    title: String,
+  },
+  components: {
+    Button,
+  },
+};
+</script>
+<style scoped>
+header {
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  margin-bottom: 20px;
+}
+</style>
---
+++ src/components/Tasks.vue
@@ -0,0 +1,21 @@
+<template>
+  <div :key ="task.id" v-for="task in tasks">
+    <!-- <h3>{{task.text}}</h3> -->
+    <Task :task="task"/>
+  </div>
+</template>
+
+<script>
+import Task from './Task'
+export default {
+  name: 'Tasks',
+  props: {
+    tasks : Array
+  },
+  
+  components:{
+    Task
+  },
+}
+console.log("this from Tasks.vue!!!!")
+</script>
